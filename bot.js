dccedea28dd4c554162af7d755b5dce53d32cb8a
hosamalden5 <45264357+hosamalden5@users.noreply.github.com>
2018-12-06 10:33:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ const spammer7 = new Discord.Client();
 const spammer8 = new Discord.Client();
 const spammer9 = new Discord.Client();
   const server = "519428982396157953";
-  const channel = "519428982396157957";
+  const channel = "520146445257408515";
 const prefix = "!!"
 spammer1.on('ready', () => {
     console.log(`Logged in as ${spammer1.user.tag}!`);
